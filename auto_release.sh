26c6a7d98c23473af1016c98f351a2989fd77710
vincent <shix_112@126.com>
2019-08-04 14:07:45 +0800

    适配 mac 的命令系统

--- auto_release.sh
+++ auto_release.sh
@@ -12,19 +12,19 @@ echo "-- "
 
 echo "      2. Start - Delete Github Dir all content"
 echo "-- "
-cd ./1_delpoy_git-github/ && ls -l | awk -F" " '{print $9}' | grep . | xargs -n1 -i rm -rf {} && cd ..
+cd ./1_delpoy_git-github/ && ls -l | awk -F" " '{print $9}' | grep . | xargs -n1 -I {} rm -rf {} && cd ..
 echo "      2. End   - Delete Github Dir all content"
 echo "-- "
 
 echo "      3. Start - Copy Tencent git to Github Dir "
 echo "-- "
-cd ./.deploy_git &&  ls -l | awk -F" " '{print $9}' | grep . | xargs -i cp -rf  {}  ../1_delpoy_git-github/ && cd ..
+cd ./.deploy_git &&  ls -l | awk -F" " '{print $9}' | grep . | xargs -I {} cp -rf  {}  ../1_delpoy_git-github/ && cd ..
 echo "      3. End   - Copy Tencent git to Github Dir "
 echo "-- "
 
 echo "      4. Start - Release all changes to Github "
 echo "-- "
-cd ./1_delpoy_git-github && git add . && date | xargs -i git commit -am "auto_commit by date of "{} && git push && cd ..
+cd ./1_delpoy_git-github && git add . && date | xargs -I {} git commit -am "auto_commit by date of "{} && git push && cd ..
 echo "      4. End    - Release all changes to Github "
 
 echo "."
